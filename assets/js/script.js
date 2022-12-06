bcd51c6ed31b52a5102132ff954ecd0ca0087b99
Malezas <lore.cont9@gmail.com>
2022-12-06 13:12:31 -0300

    navbar-scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 $(function (){
     $('[data-bs-toggle="tooltip"]').tooltip();
 });
+
+
+$(document).scroll(function(){
+    const y = $("html").scrollTop();
+
+    y > 650 ? $("nav").addClass("nav-black") : $("nav").removeClass("nav-black")
+})
